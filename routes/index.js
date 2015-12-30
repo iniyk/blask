b9aeed32c91c9afd4a73dfd8e303036788ad10b5
王梦非 <iniykfrost@outlook.com>
2015-12-30 21:01:08 +0800

    [Step] Finish data and mining page.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Panel = require("../models/Panel");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,12 +9,26 @@ router.get('/', function(req, res, next) {
 
 /* GET data management page. */
 router.get('/data', function(req, res, next) {
-    res.render('data', { project_name: 'Blask', title: 'Data Management', current_page: 'data'});
+    var datas = [];
+
+    datas.push(Panel.Panel(0, "Retial Data", "Retail market basket data", "fa fa-database fa-5x", "panel panel-info"));
+    datas.push(Panel.Panel(1, "A Data Folder", "Dataset Folder", "fa fa-folder-open fa-5x", "panel panel-primary"));
+
+    res.render('data', { project_name: 'Blask', title: 'Data Management', current_page: 'data', models: datas});
 });
 
 /* GET data mining modules page. */
 router.get('/mining', function(req, res, next) {
-    res.render('mining', { project_name: 'Blask', title: 'Data Mining', current_page: 'mining'});
+    var models = [];
+
+    models.push(Panel.Panel(0, "FP-Growth", "Frequent itemset mining algorithm", "fa fa-compress fa-5x", "panel panel-info"));
+    models.push(Panel.Panel(1, "Machine Learning", "Folder of machine learning", "fa fa-folder-open fa-5x", "panel panel-primary"));
+    models.push(Panel.Panel(2, "FP-Growth", "Frequent itemset mining algorithm", "fa fa-compress fa-5x", "panel panel-info"));
+    models.push(Panel.Panel(3, "FP-Growth very loooooooooooooooooooooog", "Frequent itemset mining algorithm", "fa fa-compress fa-5x", "panel panel-info"));
+    models.push(Panel.Panel(4, "FP-Growth short short short", "Frequent itemset mining algorithm", "fa fa-compress fa-5x", "panel panel-info"));
+    models.push(Panel.Panel(5, "FP-Growth", "Frequent itemset mining algorithm", "fa fa-compress fa-5x", "panel panel-info"));
+
+    res.render('mining', { project_name: 'Blask', title: 'Data Mining', current_page: 'mining', models: models});
 });
 
 module.exports = router;
