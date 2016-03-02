879cee1637222a7d7560c3d3a8bb2a9e75b1b400
iniyk <iniykfrost@gmail.com>
2016-03-02 19:59:15 +0800

    [Test] Add dataset upload test page.

--- models/DataController.js
+++ models/DataController.js
@@ -14,30 +14,30 @@ var logger = Logger.handle("DataController");
 var MongoController = require('./MongoController');
 
 //GET /data/:id
-router.get('/data/:id(\\d+)', function(req, res, next) {
+router.get('/:id(\\d+)', function(req, res, next) {
     //TODO 显示单个数据集
 });
 
 //GET /data/:id/edit
-router.get('/data/:id(\\d+)/edit', function(req, res, next) {
+router.get('/:id(\\d+)/edit', function(req, res, next) {
     //TODO 编辑数据集信息
 });
 
 //GET /data/:id/create
-router.get('/data/:id(\\d+)/create', function(req, res, next) {
+router.get('/:id(\\d+)/create', function(req, res, next) {
     //TODO 创建新的数据集
 });
 
 //GET /data/
-router.get('/data', function(req, res, next) {
+router.get('/', function(req, res, next) {
     //TODO 列出所有数据集
 });
 
 //POST /data/upload
-router.get('/data/upload', function (req, res, next) {
-    var name = req.files.thumbnail.name;
-    var tmp_path = req.files.thumbnail.path;
-    var type = req.files.thumbnail.type;
+router.get('/upload', function (req, res, next) {
+    var name = req.files.datafile.name;
+    var tmp_path = req.files.datafile.path;
+    var type = req.files.datafile.type;
 
     if (type == 'json') {
         common.readJson(tmp_path, function (err, json_arr) {
