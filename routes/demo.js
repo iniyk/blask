723b77c760896031919a51380e118ec00a87bbe6
iniyk <iniykfrost@outlook.com>
2016-03-09 11:06:25 +0800

    [Step] Add link to oracle modal.

--- routes/demo.js
+++ routes/demo.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var router = express.Router();
 
-var _ = require("underscore");
+var _ = require('underscore');
 
 var router_now = '/demo/';
 
@@ -12,32 +12,32 @@ var pages = {
     index: {
         name: 'index',
         title: '首页',
-        icon: "glyphicon glyphicon-home"
+        icon: 'glyphicon glyphicon-home'
     },
     source: {
         name: 'source',
         title: '数据源管理',
-        icon: "fa fa-database"
+        icon: 'fa fa-database'
     },
     exchange: {
         name: 'exchange',
         title: '数据转换',
-        icon: "fa fa-random"
+        icon: 'fa fa-random'
     },
     model: {
         name: 'model',
         title: '数据挖掘',
-        icon: "fa fa-dollar"
+        icon: 'fa fa-dollar'
     },
     schema: {
         name: 'schema',
         title: '元数据管理',
-        icon: "fa fa-table"
+        icon: 'fa fa-table'
     },
     display: {
         name: 'display',
         title: '数据展示',
-        icon: "fa fa-bar-chart"
+        icon: 'fa fa-bar-chart'
     }
 };
 
@@ -62,27 +62,49 @@ pages.source['sidebar'] = {
         name: 'add',
         title: '添加数据源',
         href: '#',
-        icon: "fa fa-plus",
+        icon: 'fa fa-plus',
         items: {
             oracle: {
                 name: 'add-oracle',
-                title: "Oracle数据源",
+                title: 'Oracle数据源',
                 href: '#',
-                icon: "fa fa-circle-o"
+                icon: 'fa fa-circle-o',
+                attrs: {
+                    'data-toggle': 'modal',
+                    'data-target': '#modelAddMysql'
+                }
             },
             mysql: {
                 name: 'add-mysql',
-                title: "MySQL数据源",
+                title: 'MySQL数据源',
                 href: '#',
-                icon: "fa fa-circle-o"
+                icon: 'fa fa-circle-o'
             },
             mongodb: {
                 name: 'add-mongodb',
-                title: "MongoDB数据源",
+                title: 'MongoDB数据源',
                 href: '#',
-                icon: "fa fa-circle-o"
+                icon: 'fa fa-circle-o'
             }
         }
+    },
+    showall: {
+        name: 'showall',
+        title: '显示全部数据源',
+        href: '#',
+        icon: 'fa fa-folder-open'
+    },
+    upload: {
+        name: 'upload',
+        title: '由文件上传',
+        href: '#',
+        icon: 'fa fa-upload'
+    },
+    settings: {
+        name: 'settings',
+        title: '设置',
+        href: '#',
+        icon: 'fa fa-gear'
     }
 };
 
@@ -93,7 +115,7 @@ router.get('/', function(req, res, next) {
         page[attr] = value;
     });
     page.pages = pages;
-    res.render('demo/index', {page: page});
+    res.render('demo/main', {page: page});
 });
 
 //GET all page include index
@@ -104,7 +126,7 @@ _.map(pages, function (one_in_pages, index) {
     });
     page.pages = pages;
     router.get('/' + page.name, function(req, res, next) {
-        res.render('demo/index', {page: page});
+        res.render('demo/main', {page: page});
     });
 });
 
