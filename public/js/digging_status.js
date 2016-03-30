aad753e20c70199a80a8fbee93c1b38c8af656de
iniyk <iniykfrost@outlook.com>
2016-03-30 11:23:48 +0800

    [Step] Running output box finished.

--- public/js/digging_status.js
+++ public/js/digging_status.js
@@ -1,6 +1,8 @@
 /**
  * Created by iniyk on 16/3/29.
  */
+var debug_running;
+
 $(document).ready(function () {
     getData(function(running, helper) {
         showInWebSite(running, helper);
@@ -16,6 +18,7 @@ function getData(callback) {
 }
 
 function showInWebSite(running, helper) {
+    debug_running = running;
     $('#helper-text').text(helper['text']);
     $('#type').text(running['type']);
     //$('#exec').text(running['exec']);
@@ -29,7 +32,34 @@ function showInWebSite(running, helper) {
                 <td>${argument.text}</td>
                 <td>${running.input[argument.name]}</td>
             </tr>
-        `
+        `;
         $('#arguments-content').append(content);
     }
+
+    for (var field of helper['outputs']) {
+        var th = `
+            <th>
+                ${field.text}
+            </th>
+        `;
+        $('#output-fields-content').append(th);
+    }
+
+    var record_number = 0;
+    for (var field_name in running['output']) {
+        var field = running['output'][field_name];
+        record_number = record_number > field.length ? record_number : field.length;
+    }
+    for (var index=0; index<record_number; index++) {
+        var tr = '';
+        for (var field_name in running['output']) {
+            var field = running['output'][field_name];
+            tr += `
+                <td>${field[index]}</td>
+            `;
+        }
+        tr = `<tr>${tr}</tr>`;
+        $('#output-records-content').append(tr);
+    }
+    $('#result-table').DataTable();
 }
