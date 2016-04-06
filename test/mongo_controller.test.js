a2241454400abdee472397e78871ad2d0e5e2d5d
iniyk <iniykfrost@gmail.com>
2016-04-06 10:50:29 +0800

    [Step] Add data upload test.

--- test/mongo_controller.test.js
+++ test/mongo_controller.test.js
@@ -16,7 +16,7 @@ var common = require('../models/Common');
 
 describe('mongo_controller.test.js - MongoController Test', function() {
     it('Array saving test', function (done) {
-        var config_test = require("../models/Common").readJsonConfig('./conf.json')['database']['mongodb_test'];
+        /*var config_test = require("../models/Common").readJsonConfig('./conf.json')['database']['mongodb_test'];
         var db_test = mongoose.createConnection(config_test.host);
         db_test.on('error', function (err) {
             should.not.exist(err);
@@ -41,63 +41,63 @@ describe('mongo_controller.test.js - MongoController Test', function() {
         model_with_array.save(function(err) {
             should.not.exist(err);
             done();
-        });
-        //var data_post = {
-        //    name: 'apriori',
-        //    data:
-        //    {
-        //        pre:
-        //            [
-        //
-        //                ["ASIAN"],
-        //                ["BLACK"],
-        //                ["Brooklyn"],
-        //                ["HISPANIC"],
-        //                ["New York"],
-        //                ["New York"],
-        //                ["NON-MINORITY"],
-        //                ["NON-MINORITY"],
-        //                ["WBE"],
-        //                ["WBE"]
-        //            ],
-        //        post:
-        //            [
-        //                ["MBE"],
-        //                ["MBE"],
-        //                ["MBE"],
-        //                ["MBE"],
-        //                ["MBE"],
-        //                ["WBE"],
-        //                ["WBE"],
-        //                ["WBE"],
-        //                ["NON-MINORITY"],
-        //                ["NON-MINORITY"]
-        //            ],
-        //        confidence:
-        //            [
-        //                '0.577565632458234',
-        //                '0.594272076372315',
-        //                '0.628318584070796',
-        //                '0.674698795180723',
-        //                '0.740740740740741',
-        //                '0.989547038327526',
-        //                '1',
-        //                '1',
-        //                '1',
-        //                '1'
-        //            ]
-        //    }
-        //};
-        //
-        //request
-        //    .post('http://localhost:3000/data/create')
-        //    .send(data_post)
-        //    .end(function(err, res){
-        //        if (err) done(err);
-        //        res.body.should.have.property('status');
-        //        res.body['status'].should.equal('success');
-        //        done();
-        //    });
+        });*/
+        var data_post = {
+            name: 'apriori_bug',
+            data:
+            {
+                pre:
+                    [
+
+                        ["ASIAN"],
+                        ["BLACK"],
+                        ["Brooklyn"],
+                        ["HISPANIC"],
+                        ["New York"],
+                        ["New York"],
+                        ["NON-MINORITY"],
+                        ["NON-MINORITY"],
+                        ["WBE"],
+                        ["WBE"]
+                    ],
+                post:
+                    [
+                        ["MBE"],
+                        ["MBE"],
+                        ["MBE"],
+                        ["MBE"],
+                        ["MBE"],
+                        ["WBE"],
+                        ["WBE"],
+                        ["WBE"],
+                        ["NON-MINORITY"],
+                        ["NON-MINORITY"]
+                    ],
+                confidence:
+                    [
+                        '0.577565632458234',
+                        '0.594272076372315',
+                        '0.628318584070796',
+                        '0.674698795180723',
+                        '0.740740740740741',
+                        '0.989547038327526',
+                        '1',
+                        '1',
+                        '1',
+                        '1'
+                    ]
+            }
+        };
+
+        request
+            .post('http://localhost:3000/data/create')
+            .send(data_post)
+            .end(function(err, res){
+                if (err) done(err);
+                res.body.should.have.property('status');
+                res.body['status'].should.equal('success');
+                done();
+            });
 
 /*
         var name = data_post.name;
